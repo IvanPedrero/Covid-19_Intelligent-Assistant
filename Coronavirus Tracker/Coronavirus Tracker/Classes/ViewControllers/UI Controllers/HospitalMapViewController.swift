be321aea23f5cdfae2ca3eac55d5e20df982d7ec
Ivan Pedrero <pedrero.ivan@hotmail.com>
2020-08-03 18:34:53 -0500

    Added map functionality.

--- Coronavirus Tracker/Coronavirus Tracker/Classes/ViewControllers/UI Controllers/HospitalMapViewController.swift
+++ Coronavirus Tracker/Coronavirus Tracker/Classes/ViewControllers/UI Controllers/HospitalMapViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 
 class MapPin: NSObject, MKAnnotation {
     let title: String?
@@ -23,23 +23,63 @@ class MapPin: NSObject, MKAnnotation {
 }
 
 class HospitalMapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK:- UI view containers.
+    @IBOutlet weak var generalTopContainerView: UIView!
+    @IBOutlet weak var backButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        queryNearbyHospitals()
+        styleContainerViews()
+        initMaps()
     }
     
-    func queryNearbyHospitals(){
+    func styleContainerViews(){
+        generalTopContainerView.layer.shadowColor = UIColor.black.cgColor
+        generalTopContainerView.layer.shadowOpacity = 0.2
+        generalTopContainerView.layer.shadowOffset = .zero
+        generalTopContainerView.layer.shadowRadius = 10
+        
+        backButton.layer.cornerRadius = 8
+        backButton.layer.shadowColor = UIColor.black.cgColor
+        backButton.layer.shadowOpacity = 0.2
+        backButton.layer.shadowOffset = .zero
+        backButton.layer.shadowRadius = 5
+    }
+    
+    func initMaps(){
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            mapView.showsUserLocation = true
+            mapView.delegate = self
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+                        
+        }else{
+            dismiss (animated: true, completion: nil)
+        }
+    }
+    
+    func queryNearbyHospitals(region:MKCoordinateRegion){
         let searchRequest = MKLocalSearch.Request()
         searchRequest.naturalLanguageQuery = "hospital"
 
 
         // Set the region to an associated map view's region.
-        searchRequest.region = mapView.region
+        searchRequest.region = region
 
 
         let search = MKLocalSearch(request: searchRequest)
@@ -61,14 +101,34 @@ class HospitalMapViewController: UIViewController {
     
     func setPinUsingMKAnnotation(title:String, location: CLLocationCoordinate2D) {
        let pin1 = MapPin(title: title, locationName: "", coordinate: location)
-       let coordinateRegion = MKCoordinateRegion(center: pin1.coordinate, latitudinalMeters: 800, longitudinalMeters: 800)
-       mapView.setRegion(coordinateRegion, animated: true)
        mapView.addAnnotations([pin1])
     }
     
     
     @IBAction func dismissView(_ sender: Any) {
        dismiss (animated: true, completion: nil)
+    }
+    
+    
+    // MARK:- External maps managing.
+    func askForMapOpening(coordinates:CLLocationCoordinate2D, title:String){
+        let alert = UIAlertController(title: "Opening directions to", message: title, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.destructive, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: { action in
+            self.openMapsAppWithDirections(to: coordinates, destinationName: title)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openMapsAppWithDirections(to coordinate: CLLocationCoordinate2D, destinationName name: String) {
+        let options = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = name // Provide the name of the destination in the To: field
+        mapItem.openInMaps(launchOptions: options)
     }
     
     /*
@@ -81,4 +141,34 @@ class HospitalMapViewController: UIViewController {
     }
     */
 
+}
+
+extension HospitalMapViewController: CLLocationManagerDelegate, MKMapViewDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       
+       let location = locations.last! as CLLocation
+       let currentLocation = location.coordinate
+       let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 6500, longitudinalMeters: 6500)
+        self.mapView.setRegion(coordinateRegion, animated: true)
+        self.locationManager.stopUpdatingLocation()
+        
+        self.queryNearbyHospitals(region: coordinateRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+       print(error.localizedDescription)
+    }
+    
+    
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        let annotation = view.annotation
+        if let coordinates = annotation?.coordinate{
+            if let title = annotation?.title ?? ""{
+                self.mapView.deselectAnnotation(annotation, animated: true)
+                self.askForMapOpening(coordinates: coordinates, title: title)
+            }
+        }
+    }
 }
